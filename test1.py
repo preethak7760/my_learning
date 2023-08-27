d157a1dde1dfef552c54e80e6813255413013af3
preetha <preethak7760@gmail.com>
2023-08-27 11:31:18 +0530

    set data type

---
+++ test1.py
@@ -0,0 +1,12 @@
+empty_list=[]
+print(empty_list)
+
+number_list=[1,2,3,4,5,6,7,8,9]
+print(number_list)
+
+name_list=["preetha","shana","govarsha"]
+name_list[1]="shana"
+print(name_list)
+
+for i in name_list:
+    print(i)
